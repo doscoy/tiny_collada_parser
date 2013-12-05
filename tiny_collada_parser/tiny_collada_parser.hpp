0d2e22b34adfbe7d0bf7553ec9f2e41bb1ff8a91
KANI Tetsuro <doscoy.t@gmail.com>
2013-12-05 23:15:43 +0900

    頂点と法線を持っているかの判定メソッド作成

--- tiny_collada_parser/tiny_collada_parser.hpp
+++ tiny_collada_parser/tiny_collada_parser.hpp
@@ -1,22 +1,37 @@
 //  mcp_parser.hpp
 
-#ifndef MCP_PARSER_HPP_INCLUDED
-#define MCP_PARSER_HPP_INCLUDED
+#ifndef TC_PARSER_HPP_INCLUDED
+#define TC_PARSER_HPP_INCLUDED
 
 // Include files.
 #include <vector>
 #include <cstdint>
 
-namespace mcp {
+//  無効なストライド幅
+#define TINY_COLLADA_MESH_INVARIDATE_STRIDE  (-2)
 
-//  Mesh data manager.
+namespace tc {
+
+//  Colladaメッシュデータ
 class Mesh
 {
+    //  法線を持っているか判定
+    bool hasNormal() const {
+        return stride_normal_ != TINY_COLLADA_MESH_INVARIDATE_STRIDE;
+    }
+    
+    //  頂点を持っているか判定
+    bool hasVertex() const {
+        return stride_vertex_ != TINY_COLLADA_MESH_INVARIDATE_STRIDE;
+    }
+
+
 private:
-    int32_t
+    int8_t stride_vertex_;
+    int8_t stride_normal_;
     
     std::vector<float> mesh_vertex_;
-    std::vector<float>
+    std::vector<float> mesh_normal_;
 
     float transform_matrix_[4][4];
 };
@@ -29,7 +44,7 @@ public:
     ~Perser();
 
 public:
-    bool load
+    bool load(const char* const dae_path);
 
 private:
     std::vector<Mesh*> mesh_;
