5bc44b26205db1a855b4b621b1a92d8006424c78
KANI Tetsuro <doscoy.t@gmail.com>
2013-12-25 12:26:05 +0900

    コメント追加

--- tiny_collada_parser/samples/sample02.cpp
+++ tiny_collada_parser/samples/sample02.cpp
@@ -8,36 +8,55 @@ namespace  {
     
 const tc::Meshes* meshes_ = nullptr;
 
-
-void drawMesh()
+//----------------------------------------------------------------------
+//	メッシュの描画
+void drawMesh(std::shared_ptr<tc::Mesh> mesh)
 {
-    std::shared_ptr<tc::Mesh> mesh = meshes_->at(0);
+	//	頂点取得
     tc::Mesh::ArrayData* pos = mesh->getVertex();
 
+	//	データ設定
     glEnableClientState(GL_VERTEX_ARRAY);
     glVertexPointer(pos->stride_, GL_FLOAT, 0, pos->data_.data());
+
+	//	描画
     glDrawElements(GL_TRIANGLES, pos->indices_.size(), GL_UNSIGNED_INT, pos->indices_.data());
-    glDisableClientState(GL_VERTEX_ARRAY);
+
+	//	設定を戻す
+	glDisableClientState(GL_VERTEX_ARRAY);
 }
 
+//----------------------------------------------------------------------
+//	描画コールバック
 void display()
 {
+
+	//	画面クリア
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glEnable(GL_DEPTH_TEST);
-    drawMesh();
-    glFlush();
+
+	std::shared_ptr<tc::Mesh> mesh = meshes_->at(0);
+
+	//	カメラ設定
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0.0, 5.5, 5.5, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
+	//	メッシュ描画B
+	drawMesh(mesh);
+
+	glFlush();
 }
 
-
+//----------------------------------------------------------------------
+//	スクリーンサイズ変更コールバック
 void reshape(int width, int height)
 {
+	//	描画領域の設定変更
     glViewport(0, 0, width, height);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluPerspective(45.0, (double)width / (double)height, 0.1, 100.0);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(0.0, 5.5, 5.5, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 }
 
 }   // unname namespace
@@ -52,6 +71,8 @@ void sample02(
     //  daeを解析
     parser.parse(dae_path);
     meshes_ = parser.meshes();
+
+	//	gl設定
     int argc = 0;
     char* argv[] = {"\0","\0"};
 //    glutInit(&argc, argv);
@@ -60,5 +81,7 @@ void sample02(
     glutReshapeFunc(reshape);
     glutDisplayFunc(display);
     glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
-    glutMainLoop();
+
+	//	イベントループ
+	glutMainLoop();
 }
